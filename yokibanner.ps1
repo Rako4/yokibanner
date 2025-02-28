a53d49983e80447ad70a5b87d8bd6a7749198637
Rako4 <88365506+Rako4@users.noreply.github.com>
2025-02-28 11:05:14 -0500

    Update yokibanner.ps1

---
+++ yokibanner.ps1
@@ -0,0 +1,10 @@
+function Show-Banner {
+    Write-Host @"
+ __     __   _    _ 
+ \ \   / /  | |  (_)
+  \ \_/ /__ | | ___ 
+   \   / _ \| |/ / |
+    | | (_) |   <| |
+    |_|\___/|_|\_\_|
+"@ -ForegroundColor Cyan
+}
